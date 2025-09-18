cadf44cb53bc35cde0718c1661a405820c74d2fc
r1Way <2142341323@qq.com>
2025-09-18 11:04:13 +0800

    46

--- exercises/hashmaps/hashmaps2.rs
+++ exercises/hashmaps/hashmaps2.rs
@@ -14,7 +14,6 @@
 // Execute `rustlings hint hashmaps2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -29,14 +28,20 @@ enum Fruit {
 
 fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
     let fruit_kinds = vec![
-        Fruit::Apple,
+        Fruit::Apple,//4
         Fruit::Banana,
-        Fruit::Mango,
-        Fruit::Lychee,
+        Fruit::Mango,//2
+        Fruit::Lychee,//5
         Fruit::Pineapple,
     ];
 
     for fruit in fruit_kinds {
+      match   fruit{
+        Fruit::Apple | Fruit::Mango | Fruit::Lychee => continue,
+        _ => {
+          basket.insert(fruit,1);
+        }
+      }
         // TODO: Insert new fruits if they are not already present in the
         // basket. Note that you are not allowed to put any type of fruit that's
         // already present!
--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -12,7 +12,7 @@
 // is going to be applied to the string. It can either be:
 // - Uppercase the string
 // - Trim the string
-// - Append "bar" to the string a specified amount of times
+// - Append "bar" to the string a specified amount of times[;]
 // The exact form of this will be:
 // - The input is going to be a Vector of a 2-length tuple,
 //   the first element is the string, the second one is the command.
@@ -20,7 +20,6 @@
 //
 // No hints this time!
 
-// I AM NOT DONE
 
 pub enum Command {
     Uppercase,
@@ -32,12 +31,17 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String,Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
-        }
+            match command {
+                Command::Uppercase => output.push(string.to_uppercase()),
+                Command::Trim => output.push(string.trim().to_string()),
+                Command::Append(n) => output.push(String::from(string) + &"bar".repeat(*n)),
+            }
+          }
         output
     }
 }
@@ -45,7 +49,7 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use crate::my_module::transformer as transformer;
     use super::Command;
 
     #[test]
